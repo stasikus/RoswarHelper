9592a07e17938d9f80e7c0b2daa903b6d810df21
Stanislav Kravchenko <stasikusk@gmail.com>
2014-11-25 22:55:40 +0200

    ew

--- Requests.cs
+++ Requests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using xNet.Collections;
 using xNet.Net;
 
 namespace RoswarHelper
@@ -18,6 +19,9 @@ namespace RoswarHelper
             request.Cookies = new CookieDictionary();
             string get = request.Get("http://www.roswar.ru/login/").ToString();
             string post = request.Post("http://www.roswar.ru/login/", "action=login&email=" + System.Web.HttpUtility.UrlEncode(login) + "&password=" + System.Web.HttpUtility.UrlEncode(pass, Encoding.GetEncoding(1251)) + "&remember=on", "application/x-www-form-urlencoded").ToString();
+            StreamWriter sw = new StreamWriter("D:\\1\\1.txt", true, System.Text.Encoding.GetEncoding(1251));
+            sw.Write(post);
+            sw.Close();
 
             return request;
         }
@@ -26,13 +30,26 @@ namespace RoswarHelper
         {
             request.AddField("X-Requested-With", "XMLHttpRequest");
             string post = request.Post("http://www.roswar.ru/player/restorehp/", "action=restorehp", "application/x-www-form-urlencoded").ToString();
+
+            StreamWriter sw = new StreamWriter("D:\\1\\2.txt",true,System.Text.Encoding.GetEncoding(1251));
+            sw.Write(post);
+            sw.Close();
         }
 
         public static int spendGold(HttpRequest request, int playType)
         {
             int goldLefet = 0;
-            request.AddField("X-Requested-With", "XMLHttpRequest");
-            string post = request.Post("http://www.roswar.ru/camp/gypsy/", "action=gypsyStart&gametype=" + playType + "", "application/x-www-form-urlencoded").ToString();
+            request.AddHeader("X-Requested-With", "XMLHttpRequest");
+
+            var urlParams = new RequestParams();
+            urlParams["action"] = "gypsyStart";
+            urlParams["gametype"] = playType;
+
+            string post = request.Post("http://www.roswar.ru/camp/gypsy/", urlParams.ToString(), "application/x-www-form-urlencoded").ToString();
+
+            StreamWriter sw = new StreamWriter("D:\\1\\3.txt", true, System.Text.Encoding.GetEncoding(1251));
+            sw.Write(post);
+            sw.Close();
         
             //string post = request.Post("http://www.roswar.ru/camp/gypsy/", "action=gypsyAuto", "application/x-www-form-urlencoded").ToString();
 
